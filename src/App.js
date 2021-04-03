ce982e8cc23865315f7cec7f37be612b63f2b32a
Biniam <Biniamt23@yahoo.com>
2021-04-02 21:48:57 -0700

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeScreen from './HomeScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Buildig netflix</h1>
+      <HomeScreen/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDT-Rmt7jTbpg-w7SDcDrrrAWnC9ty4GYc",
+    authDomain: "netflixreactapp-e9ea8.firebaseapp.com",
+    projectId: "netflixreactapp-e9ea8",
+    storageBucket: "netflixreactapp-e9ea8.appspot.com",
+    messagingSenderId: "839369404898",
+    appId: "1:839369404898:web:5196a0feaac219d76889bb"
+  };
