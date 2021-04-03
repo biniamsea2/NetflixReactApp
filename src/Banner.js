ef8972cd67800587ddd49c7c0f6e651fed93fe1c
Biniam <Biniamt23@yahoo.com>
2021-04-03 00:31:00 -0700

    working on css for banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import './Banner.css';
 
 function Banner() {
-    return (
-        <div>
-            
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAT4AAACfCAMAAABX0UX9AAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABODcYhAAEl463hAAAAAElFTkSuQmCC")`,
+        // backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+        backgroundPosition: "center center",
+
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
         </div>
-    )
+        <h1 className="banner_description">This is a test description</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
