9318a66165de912aa8e5f5d6d09599703d6f3875
Biniam <Biniamt23@yahoo.com>
2021-04-04 20:53:48 -0700

    name and movie description now displaying

--- src/Banner.js
+++ src/Banner.js
@@ -38,24 +38,15 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">Movie Name</h1>
+        {/* could be labled as title or name */}
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">
-          {truncate(
-            `This is a test descriptis a test descriptis a test dst descriptis a tes
-        t descriptis a test dst descriptis a test descriptis a test descriptis a test descriptis a test descriptis a test
-         descriptis a test descriptis a riptis a test descriptis a test dst descriptis a tes
-         t descriptis a test dst descriptis a test descriptis a test descriptis a test descriptis a test descriptis a test
-          descriptis a test descriptis a riptis a test descriptis a test dst descriptis a tes
-          t descriptis a test dst descriptis a test descriptis a test descriptis a test descriptis a test descriptis a test
-           descriptis a test descriptis a test descriptis a test descriptis a test descriptis a test descriptis a test descri
-         ptis a test description`,
-            150
-          )}
-        </h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
