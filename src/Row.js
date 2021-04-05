c2977652259ef62365aa322a20fd0850fd8a2eb0
Biniam <Biniamt23@yahoo.com>
2021-04-04 23:26:32 -0700

    ran into error for display images for each row, now working had wrong url for images and posters needed to copy same url for backdrop in header

--- src/Row.js
+++ src/Row.js
@@ -5,6 +5,8 @@ import axios from "./axios";
 function Row({ title, getUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(getUrl);
@@ -13,12 +15,21 @@ function Row({ title, getUrl, isLargeRow = false }) {
     }
 
     fetchData();
+    // dependency: [getUrl]
   }, [getUrl]);
-  console.log(movies);
 
   return (
     <div className="row">
       <h2>{title}</h2>
+      {movies.map((movie) => (
+        <img
+          src={`${base_url}${
+              //if movie is large display poster otherwise display backdrop
+            isLargeRow ? movie.poster_path : movie.backdrop_path
+          }`}
+          alt={movie.name}
+        />
+      ))}
     </div>
   );
 }
