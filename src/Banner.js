aa9074856227e986773deb22ca3a017518e25d2c
Biniam <Biniamt23@yahoo.com>
2021-04-04 22:43:21 -0700

    still having issue with image cutting off on banner, going to move on and comeback, working on building the rows of categories

--- src/Banner.js
+++ src/Banner.js
@@ -32,7 +32,7 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        // tmdb gives us this url to be able to use as image in our app.. we add an optional(?) because in the beginning the movie isnt going to be defined
+        // tmdb gives us this url to be able to use as image in our app.. we add an optional(?) because in the beginning the movie isnt going to be defined, it could be undefined
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
@@ -46,6 +46,7 @@ function Banner() {
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
+        {/* movie description comes back as "overview" */}
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className="banner--fadeBottom" />
