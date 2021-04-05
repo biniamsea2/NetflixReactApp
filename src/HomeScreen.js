dde427152ff8b5194284b328ca94e22b1a23c954
Biniam <Biniamt23@yahoo.com>
2021-04-04 23:06:26 -0700

    row titles added and able to console log movies for each row

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
 import Row from "./Row";
+import requests from "./Requests";
 
 function HomeScreen() {
-    return (
-        <div className="homeScreen">
-            <Nav/>
-            <Banner/>
-            <Row/>
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <Banner />
 
-            {/* Row */}
-            
-        </div>
-    )
+      <Row
+        title="NETFLIX ORIGINALS"
+        getUrl={requests.getNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" getUrl={requests.getTrending} />
+      <Row title="Action Movies" getUrl={requests.getActionMovies} />
+      <Row title="Comedy Movie" getUrl={requests.getComedyMovies} />
+      <Row title="Horror Movies" getUrl={requests.getHorrorMovies} />
+      <Row title="Romance Movies" getUrl={requests.getRomanceMovies} />
+      <Row title="Documentaries" getUrl={requests.getDocumentaries} />
+
+      {/* Row */}
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
