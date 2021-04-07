ecdd1d7156ff55c018887a485f993eab143ce91b
Biniam <Biniamt23@yahoo.com>
2021-04-07 13:41:00 -0700

    user login working now, had error in store.js file, forgot to change name to user

--- src/features/UserSlice.js
+++ src/features/UserSlice.js
@@ -1,17 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     user: null,
   },
   reducers: {
-    login:(state, action) =>{
+    login: (state, action) =>{
       state.user = action.payload;
     },
     logout : (state) =>{
       state.user = null;
-    }
+    },
   },
 });
 
@@ -21,6 +21,6 @@ export const { login, logout } = userSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectUser = state => state.user.user;
+export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
