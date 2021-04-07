ce95cbd69bd16cfe4a6f1a9edbbcc93457f09d05
Biniam <Biniamt23@yahoo.com>
2021-04-06 18:28:22 -0700

    fixed file setup and new folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import HomeScreen from './HomeScreen';
-import './App.css';
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen/>
-
+      <Router>
+        <Switch>
+          {/* only render homescreen if its the exact path */}
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
