f2651fb1feaeef7f3fadaff6739175a741ca16a8
Biniam <Biniamt23@yahoo.com>
2021-04-06 19:16:23 -0700

    working on css and input form for profile route

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -12,6 +12,20 @@ function LoginScreen() {
         <button className="loginScreen_button">Sign In</button>
 
         <div className="loginScreen_gradient"/>
+        <div className="loginScreen_body">
+            <>
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+            <div className="loginScreen_input">
+                <form >
+                    <input type="email" placeholder="Email Address"/>
+                    <button className="loginScreen_GetStarted">Get Started</button>
+                </form>
+            </div>
+            
+            </>
+        </div>
       </div>
     </div>
   );
