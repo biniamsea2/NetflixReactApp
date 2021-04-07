3995f32978414eedf89729f3aeb8e841d0cb2672
Biniam <Biniamt23@yahoo.com>
2021-04-06 22:57:31 -0700

    auth added and connected to db, working on presistence

--- src/firebase.js
+++ src/firebase.js
@@ -8,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "839369404898",
     appId: "1:839369404898:web:5196a0feaac219d76889bb"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+// you can only have one default export
+  export {auth};
+  export default db;
