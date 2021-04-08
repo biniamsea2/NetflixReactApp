8d2109757b48f66b0aec606013ce6cadcf4128b3
Biniam <Biniamt23@yahoo.com>
2021-04-07 20:08:35 -0700

    sign out button added to profileEdit page button include history.push

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import Nav from '../Nav';
+import React from "react";
+import { selectUser } from "../features/UserSlice";
+import { useSelector } from "react-redux";
+import Nav from "../Nav";
 import "./ProfileScreen.css";
+import { auth } from "../firebase";
 
 function ProfileScreen() {
-    return (
-        <div className="profileScreen">
-            <Nav/>
+  const user = useSelector(selectUser);
+
+  return (
+    <div className="profileScreen">
+      <Nav />
+      <div className="profileScreen_body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen_info">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt="avatar logo"
+          />
         </div>
-    )
+        <div className="profileScreen_details">
+          <h2>{user.email}</h2>
+          <div className="profileScreen_plans">
+            <button
+              onClick={() => auth.signOut()}
+              className="profileScreen_signOut"
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfileScreen
+export default ProfileScreen;
