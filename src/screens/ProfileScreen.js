1bd9b20a7619a3934584b30cc5bfc1de0fd133bc
Biniam <Biniamt23@yahoo.com>
2021-04-14 19:13:57 -0700

    added modal and movie name appearing now, going to work on trailer

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -13,7 +13,7 @@ function ProfileScreen() {
     <div className="profileScreen">
       <Nav />
       <div className="profileScreen_body">
-        <h1>Edit Profile</h1>
+        <h1>Profile</h1>
         <div className="profileScreen_info">
           <img
             src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
