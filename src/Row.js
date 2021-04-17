687ab0fe372116aa0dae54241cccd0f3bd66568c
Biniam <Biniamt23@yahoo.com>
2021-04-16 20:18:30 -0700

    changed names for variables and methods

--- src/Row.js
+++ src/Row.js
@@ -8,67 +8,42 @@ import ReactPlayer from "react-player";
 function Row({ title, getUrl }) {
   const [movies, setMovies] = useState([]);
   const [getMovie, setGetMovie] = useState("");
-  const [getTrailer, setTrailertest] = useState("");
-  const [getmovieTrialer, setMovieTRailer] = useState("");
-  const [getallTogether, setAllTogether] = useState("");
+  const [getAllMovieInfo, setAllMovieInfo] = useState("");
+  const [getmovieTrialer, setMovieTrailer] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
-
   const base_url = "https://image.tmdb.org/t/p/original/";
-  const urltest = `https://api.themoviedb.org/3/movie/${getMovie?.id}/videos?api_key=${process.env.REACT_APP_API_KEY}`;
-  const final = `https://www.youtube.com/watch?v=${getmovieTrialer?.key}`;
+  // query to get all info regarding a specific movie
+  const allMovieInfo = `http://api.themoviedb.org/3/movie/${getMovie?.id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`;
+  // query to movie trailer with key
+  const trailerLink = `https://www.youtube.com/watch?v=${getmovieTrialer?.key}`;
 
-  const allTogether = `http://api.themoviedb.org/3/movie/${getMovie?.id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`;
-
-
-  // const newTrailer = `https://www.youtube.com/watch?v=${getallTogether?.videos.results.key}`;
-
-  
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(getUrl);
       setMovies(request.data.results);
       return request;
     }
-
     fetchData();
     // dependency: [getUrl]
   }, [getUrl]);
 
-
   useEffect(() => {
-    async function fetchMovieData() {
-      const movieInfo = await axios.get(allTogether);
-      setAllTogether(movieInfo?.data);
+    async function fetchAllMovieInfo() {
+      const movieInfo = await axios.get(allMovieInfo);
+      setAllMovieInfo(movieInfo?.data);
       return movieInfo;
     }
-
-    fetchMovieData();
+    fetchAllMovieInfo();
   });
 
   useEffect(() => {
-    async function fetchMovieDataTrailer() {
-      const movieInfo2 = await axios.get(allTogether);
-      setMovieTRailer(movieInfo2?.data.videos.results[0]);
+    async function fetchMovieTrailer() {
+      const movieInfo2 = await axios.get(allMovieInfo);
+      setMovieTrailer(movieInfo2?.data.videos.results[0]);
       return movieInfo2;
     }
-
-    fetchMovieDataTrailer();
+    fetchMovieTrailer();
   });
-
-
-
-  
-  useEffect(() => {
-    async function fetchMovieKeyTrailer() {
-      const movieTrailerKey = await axios.get(urltest);
-      setTrailertest(movieTrailerKey?.data.results[0]);
-      return movieTrailerKey;
-    }
-
-    fetchMovieKeyTrailer();
-  });
-
-
 
   return (
     <div className="row">
@@ -108,20 +83,26 @@ function Row({ title, getUrl }) {
             },
           }}
         >
-          <ReactPlayer url={final} width="100%" height="75%" controls={true} />
+          <ReactPlayer
+            url={trailerLink}
+            width="100%"
+            height="75%"
+            controls={true}
+          />
 
           <h1 className="modal_title">
-            {getallTogether?.name || getallTogether?.title || getallTogether?.original_name}
+            {getAllMovieInfo?.name ||
+              getAllMovieInfo?.title ||
+              getAllMovieInfo?.original_name}
           </h1>
           <h1 className="modal_year_description">
-            {dateFormat(getallTogether?.release_date, "yyyy")}
+            {dateFormat(getAllMovieInfo?.release_date, "yyyy")}
           </h1>
-          <h1>{getallTogether?.runtime}</h1>
-          <h1 className="modal_year_description">{getallTogether?.overview}</h1>
-          {/* <h1>{getallTogether?.popularity}</h1> */}
-
-          {/* <h1>{getallTogether?.videos.results[0]?.key}</h1> */}
-          <h1>{getallTogether?.vote_average}</h1>
+          <h1>{getAllMovieInfo?.runtime}</h1>
+          <h1 className="modal_year_description">
+            {getAllMovieInfo?.overview}
+          </h1>
+          <h1>{getAllMovieInfo?.vote_average}</h1>
 
           <div>
             <button
