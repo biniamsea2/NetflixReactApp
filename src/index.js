2fc7d8d549f0a6549b45497502db7d61b8e0af6c
Biniam <Biniamt23@yahoo.com>
2021-04-18 00:37:04 -0700

    was able to get loading indicator to work while waiting for response for youtube video, need to work on position though

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import "react-circular-progressbar/dist/styles.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
